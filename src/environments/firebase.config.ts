ad24aece85eb35397dde3a19251d032848a37b45
deejema <deejema@users.noreply.github.com>
2018-06-23 00:06:31 -0700

    Update firebase.config.ts

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -3,10 +3,10 @@
 */
 
 export const firebaseConfig = {
-	apiKey: "AIzaSyCF56ydFPLBop0LDn-Ly12IkYfejkPsqTk",
-	authDomain: "chat-e985c.firebaseapp.com",
-	databaseURL: "https://chat-e985c.firebaseio.com",
-	projectId: "chat-e985c",
-	storageBucket: "chat-e985c.appspot.com",
-	messagingSenderId: "395949249792"
+	apiKey: "xxxxxxxxxx",
+	authDomain: "xxxxxxxx",
+	databaseURL: "xxxxxxxxx",
+	projectId: "xxxxxxxx",
+	storageBucket: "xxxxxxxxxx",
+	messagingSenderId: "xxxxxxxx"
 };
